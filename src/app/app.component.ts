8b0660518915a958b8d310f84bc3deed05f0fd56
unknown <subhramira@gmail.com>
2020-01-11 22:09:35 +0530

    store tasks at local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,100 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tm';
+  dummyTask = [
+    {
+      name: 'assign tasks',
+      description: 'only for loged userdd',
+      dueDate: '2020-01-22T06:00:17.000Z',
+      assignedTo: 2,
+      status: 'IN_PROGRESS'
+    },
+    {
+      name: 'delete task',
+      description: 'delete user',
+      dueDate: '2020-01-11T06:33:54.820Z',
+      assignedTo: 2,
+      status: 'IN_PROGRESS'
+    },
+    {
+      name: 'create task',
+      description: 'created by duper user',
+      dueDate: '2020-01-21T07:35:46.000Z',
+      assignedTo: 2,
+      status: 'IN_PROGRESS'
+    },
+    {
+      name: 'add duper user',
+      description: 'duper user',
+      dueDate: '2020-01-11T09:02:39.006Z',
+      assignedTo: 2,
+      status: 'NEW'
+    },
+    {
+      name: 'grant acess',
+      description: 'grant acess to end user',
+      dueDate: '2020-01-11T12:56:46.867Z',
+      assignedTo: 2,
+      status: 'NEW'
+    },
+     {
+      name: 'add super user',
+      description: 'super user added ',
+      dueDate: '2020-01-21T07:35:46.000Z',
+      assignedTo: 2,
+      status: 'IN_PROGRESS'
+    },
+    {
+      name: 'Create multiple task',
+      description: 'create multiple task for all user',
+      dueDate: '2020-01-11T09:02:39.006Z',
+      assignedTo: 2,
+      status: 'NEW'
+    },
+    {
+      name: 'Add timesheet table',
+      description: 'timesheett table for esch duper user',
+      dueDate: '2020-01-11T12:56:46.867Z',
+      assignedTo: 2,
+      status: 'NEW'
+    },
+     {
+      name: 'Grant permission',
+      description: 'Grant permission to duper user if condition satisfies',
+      dueDate: '2020-01-21T07:35:46.000Z',
+      assignedTo: 2,
+      status: 'IN_PROGRESS'
+    },
+    {
+      name: 'Create login page',
+      description: 'login page with validation for userid and password',
+      dueDate: '2020-01-11T09:02:39.006Z',
+      assignedTo: 2,
+      status: 'NEW'
+    },
+    {
+      name: 'create task page',
+      description: 'Create material table for task',
+      dueDate: '2020-01-11T12:56:46.867Z',
+      assignedTo: 2,
+      status: 'NEW'
+    },
+    {
+      name: 'Update login page',
+      description: 'login page with validation for userid and password',
+      dueDate: '2020-01-11T09:02:39.006Z',
+      assignedTo: 2,
+      status: 'NEW'
+    },
+    {
+      name: 'Update task page',
+      description: 'Create material table for task',
+      dueDate: '2020-01-11T12:56:46.867Z',
+      assignedTo: 2,
+      status: 'NEW'
+    }
+  ];
+  constructor() {
+    localStorage.setItem('tasks', JSON.stringify(this.dummyTask));
+  }
 }
--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -15,86 +15,6 @@ export class TaskComponent implements OnInit, AfterViewInit {
   displayData = [];
   displayedColumns: string[] = ['name', 'description', 'duedate', 'action'];
   dataSource;
-  dummyTask = [
-    {
-      name: 'assign tasks',
-      description: 'only for loged userdd',
-      dueDate: '2020-01-22T06:00:17.000Z',
-      assignedTo: 2,
-      status: 'IN_PROGRESS'
-    },
-    {
-      name: 'delete task',
-      description: 'delete user',
-      dueDate: '2020-01-11T06:33:54.820Z',
-      assignedTo: 2,
-      status: 'IN_PROGRESS'
-    },
-    {
-      name: 'create task',
-      description: 'created by duper user',
-      dueDate: '2020-01-21T07:35:46.000Z',
-      assignedTo: 2,
-      status: 'IN_PROGRESS'
-    },
-    {
-      name: 'add duper user',
-      description: 'duper user',
-      dueDate: '2020-01-11T09:02:39.006Z',
-      assignedTo: 2,
-      status: 'NEW'
-    },
-    {
-      name: 'grant acess',
-      description: 'grant acess to end user',
-      dueDate: '2020-01-11T12:56:46.867Z',
-      assignedTo: 2,
-      status: 'NEW'
-    },
-     {
-      name: 'add super user',
-      description: 'super user added ',
-      dueDate: '2020-01-21T07:35:46.000Z',
-      assignedTo: 2,
-      status: 'IN_PROGRESS'
-    },
-    {
-      name: 'Create multiple task',
-      description: 'create multiple task for all user',
-      dueDate: '2020-01-11T09:02:39.006Z',
-      assignedTo: 2,
-      status: 'NEW'
-    },
-    {
-      name: 'Add timesheet table',
-      description: 'timesheett table for esch duper user',
-      dueDate: '2020-01-11T12:56:46.867Z',
-      assignedTo: 2,
-      status: 'NEW'
-    },
-     {
-      name: 'Grant permission',
-      description: 'Grant permission to duper user if condition satisfies',
-      dueDate: '2020-01-21T07:35:46.000Z',
-      assignedTo: 2,
-      status: 'IN_PROGRESS'
-    },
-    {
-      name: 'Create login page',
-      description: 'login page with validation for userid and password',
-      dueDate: '2020-01-11T09:02:39.006Z',
-      assignedTo: 2,
-      status: 'NEW'
-    },
-    {
-      name: 'create task page',
-      description: 'Create material table for task',
-      dueDate: '2020-01-11T12:56:46.867Z',
-      assignedTo: 2,
-      status: 'NEW'
-    }
-  ];
-
   @ViewChild(MatSort, { static: true }) sort: MatSort;
   getAllUser: User[];
   rowCount = 10;
@@ -106,7 +26,6 @@ export class TaskComponent implements OnInit, AfterViewInit {
               private route: ActivatedRoute,
               public dialog: MatDialog,
               private snackBar: MatSnackBar) {
-    localStorage.setItem('tasks', JSON.stringify(this.dummyTask));
     this.getAllUser = JSON.parse(localStorage.getItem('users'));
 
   }
